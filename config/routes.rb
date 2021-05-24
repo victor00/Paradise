6b717cc7a3167f0440486f1dca15d169c7f6462c
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-05-24 16:44:06 -0300

    created api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '/api', defaults: { format: :json } do
+    resources :users, only: %i[index create show]
+    resources :messages, only: %i[index create show]
+    resources :week_points, only: %i[index create show]
+  end
 end

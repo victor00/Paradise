255bd2e44dd245f5f827acf5b66ec524fa6a4170
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-05-24 16:49:43 -0300

    added new validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :messages
   has_many :week_points
+
+  validates :name, presence: true
 end
--- app/models/week_point.rb
+++ app/models/week_point.rb
@@ -1,3 +1,9 @@
 class WeekPoint < ApplicationRecord
   belongs_to :user
+
+  validates :points, presence: true
+
+  def cap_points?
+    points <= 250
+  end
 end
